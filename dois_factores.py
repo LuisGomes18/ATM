4ed82e9587b88e1358910b8a969872ce4ecc8d2b
LuisGomes18 <97010136+LuisGomes18@users.noreply.github.com>
2023-06-16 11:27:16 +0100

    Refatorar importações

--- dois_factores.py
+++ dois_factores.py
@@ -3,9 +3,10 @@ Instala o modulo paras gerar numero aleatorio
 Instala o modulo para verificar se ficheiro existe e apagalo caso necessario
 Instala o modulo pqra fazer qrcodes
 """
-import random
-import os
-import qrcode  # pylint: disable=E0401
+from random import randint
+from os import path
+from os import remove
+from qrcode import make  # pylint: disable=E0401
 
 DEBUG = 0
 
@@ -14,13 +15,13 @@ def metodo_dois_factores():
     Função de dois factores
     '''
     while True:
-        numero_sorteado = random.randint(100000, 999999)
-        if os.path.exists("Dois_Factores.png"):
-            os.remove("Dois_Factores.png")
+        numero_sorteado = randint(100000, 999999)
+        if path.exists("Dois_Factores.png"):
+            remove("Dois_Factores.png")
         else:
             pass
 
-        img = qrcode.make(f'{numero_sorteado}')
+        img = make(f'{numero_sorteado}')
         type(img)
         img.save("Dois_Factores.png")
 
